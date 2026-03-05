7de2c936dfad4d4c3074dcae628499216ea54561
stalhahamid-del <stalhahamid@gmail.com>
2026-03-05 14:22:20 +0530

    inital commit

--- registry_service/registry/urls.py
+++ registry_service/registry/urls.py
@@ -1,8 +1,9 @@
 # registry/urls.py
 from django.urls import path
-from .views import register_agent, list_agents
+from .views import register_agent, list_agents,search_agents
 
 urlpatterns = [
     path("api/agents/register", register_agent),
     path("api/agents/list", list_agents),
+    path("api/agents/search", search_agents),
 ]
--- registry_service/registry/views.py
+++ registry_service/registry/views.py
@@ -17,3 +17,30 @@ def list_agents(request):
     agents = Agent.objects.all()
     serializer = AgentSerializer(agents, many=True)
     return Response(serializer.data)
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Agent
+
+@api_view(["GET"])
+def search_agents(request):
+    capability = request.GET.get("capability")
+
+    if not capability:
+        return Response({"error": "capability parameter required"}, status=400)
+
+    agents = Agent.objects.filter(capabilities__contains=[capability])
+
+    data = []
+    for agent in agents:
+        data.append({
+            "id": agent.id,
+            "name": agent.name,
+            "description": agent.description,
+            "capabilities": agent.capabilities,
+            "endpoint_url": agent.endpoint_url,
+            "status": agent.status
+        })
+
+    return Response(data)
